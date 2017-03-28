25f0f77d7a387f13756f722ebbf3fe7d6744c2f7
harleyvanselow <harleyvanselow@gmail.com>
2017-03-28 14:05:51 -0600

    Fixed p1

--- vanselow.pl
+++ vanselow.pl
@@ -1,5 +1,7 @@
 fourSquares(N,[S1,S2,S3,S4]):- between(0,N,S1),between(0,N,S2),between(0,N,S3),between(0,N,S4),
+                                S1S is S1*S1,S2S is S2*S2,S3S is S3*S3,S4S is S4*S4,
 								S1=<S2,S3=<S4,S2=<S3,
-								sum_list([S1,S2,S3,S4],N),
-								isSquare(S1),isSquare(S2),isSquare(S3),isSquare(S4).
+								sum_list([S1S,S2S,S3S,S4S],N),
+								isSquare(S1S),isSquare(S2S),isSquare(S3S),isSquare(S4S).
 isSquare(V):- N is sqrt(V), float_fractional_part(N) =:= 0.
+
